254732abf7a00e07788d2f887aed9a160fc83771
Caio Camargo <cchappie315@gmail.com>
2022-11-02 13:10:52 -0300

    ajustando cliente

--- Clientes/admin.py
+++ Clientes/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import ClientesTe
 
 
-admin.site.register(ClientesTe)
+@admin.register(ClientesTe)
 class Clientesadmin(admin.ModelAdmin):
     list_display = ['nome','cnpj', 'email',]
-    
+    search_fields = ('nome','cnpj','email',)
+
--- Clientes/models.py
+++ Clientes/models.py
@@ -2,22 +2,23 @@ from django.db import models
 
 
 class ClientesTe(models.Model):
+    cliente = models.CharField(max_length=60, verbose_name='Cliente')
     nome = models.CharField(max_length=60, verbose_name='Nome')
-    cnpj = models.CharField(max_length=14, verbose_name='CPF/CNPJ')
     email = models.EmailField(max_length=70,verbose_name='Email')
-    tel_fixo = models.CharField(max_length=14, verbose_name='Telefone',
-    null=True, blank=True)
-    tel_celular = models.CharField(max_length=14, verbose_name='Celular',
-    null=True, blank=True)
+    tel_contato = models.CharField(max_length=14, verbose_name='Telefone',)
+    cnpj = models.CharField(max_length=14, verbose_name='CPF/CNPJ',
+        null=True, blank=True)
     descricao = models.TextField(null=True, blank=True)
+    validacao_contrato = models.DateField()
+
     
     def __str__(self):
-        return self.nome
+        return self.cliente
 
     class Meta:
         verbose_name = ('Cliente')
         verbose_name_plural = ('Clientes')
-        ordering = ["nome"]
+        ordering = ["cliente"]
 
     
     
